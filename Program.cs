7d5713cf3cddd75d48ffb3858e5c05a96a0191eb
Foster90 <matthew.foster90@hotmail.com>
2020-10-15 21:14:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeNow = DateTime.Now;/*ToString("yyyy-MM-dd h:mm:ss tt");*/
+            DateTime timeWeek = DateTime.Now.AddMinutes(5);/*.ToString("dd.MM.yy");*/
+            DateTime randomdate = GetRandomDate(timeNow, timeWeek);
+
+            Console.WriteLine(timeNow);
+            Console.WriteLine(timeWeek);
+            Console.WriteLine(randomdate);
+            Console.ReadLine();
+         
+
+        }
+
+        static readonly Random rnd = new Random();
+        public static DateTime GetRandomDate(DateTime from, DateTime to)
+        {
+            var range = to - from;
+
+            var randTimeSpan = new TimeSpan((long)(rnd.NextDouble() * range.Ticks));
+
+            return from + randTimeSpan;
+        }
+    }
+}
